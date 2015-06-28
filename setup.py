a50206e01920123c745ef24968a0d4dd40f49f3a
Shoji Ihara <shoji.ihara@gmail.com>
2015-06-28 19:32:19 +0900

    added install requirements

--- setup.py
+++ setup.py
@@ -5,16 +5,18 @@ del os.link
 
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    req = f.readlines()
+
+
 setup(
     name='CuraSlicer',
     version='0.0.2',
     description='',
     packages=find_packages(),
     include_package_data=True,
-    entry_points={
-    },
-    tests_require=[
-    ],
+    install_requires=req,
+    tests_require=[],
     test_suite='nose.collector',
 )
 
